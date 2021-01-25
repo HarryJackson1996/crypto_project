a536498262ef0269db8e2a96e1d4fde36f5f4d5e
Harry Jackson <hwjackson1996@gmail.com>
2021-01-25 10:48:27 +0000

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/crytpo_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.crytpo_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/clients/crypto_client.dart
@@ -0,0 +1,3 @@
+class CryptoClient {
+  static const apiKey = 'E6E0DE7D-1527-4EC7-8450-30FA4D9328D6';
+}
